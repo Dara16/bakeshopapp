b7932cd33694bbe7725ece831f449f3b9d299089
Dara16 <just4oludee@yahoo.com>
2022-07-27 15:40:57 -0400

    created navbar with links

--- bakeshop/src/App.js
+++ bakeshop/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import NavBar from './components/NavBar';
 import CakeContainer from './components/CakeContainer';
 import './App.css';
 
@@ -8,9 +8,14 @@ export default function App() {
   return (
     <Router>
       <div className="App">
-    
+
+        <div className="navbar">
+          <NavBar />
+        </div>
+
         <Routes>
-          <Route exact path="/" element={<CakeContainer/>} />
+          
+          <Route exact path="/cakes" element={<CakeContainer />} />
         </Routes>
       </div>
     </Router>
